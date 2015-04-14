264d19607fc979b0ac9f4e858296bf4334f03eb7
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-14 00:10:09 +0000

    edit5

--- task_01.py
+++ task_01.py
@@ -14,24 +14,23 @@ def sum_orders(customers, orders):
             returns a new dictionary combined with the keys and containing
             data from two dictionaries.
       Examples:
-                  CUSTOMERS = {2: {'name': 'Person One', 'email': 'email@one.com'},
+                CUSTOMERS = {2: {'name': 'Person One', 'email': 'email@one.com'},
                       5: {'name': 'Person Two', 'email': 'email@two.com'}}
                   ORDERS = {1: {'customer_id': 2, 'total': 10},
                       3: {'customer_id': 2, 'total': 10},
                       4: {'customer_id': 3, 'total': 15}}
                   >>>
 
-    """    
+    """
 dict1 = {}
-for cust in ORDERS.itervalues():
+for cust in orders.itervalues():
     if cust['customer_id'] in dict1.keys():
         order1 = dict1[cust]['orders'] + 1
         total1 = dict1[cust]['total'] + [cust]['total']
     else:
         no_of_orders = 1
         order_total = [cust]['total']
-        name1 = customers[keys]['name']
-        email1 = customers[keys]['email']
+        name1 = customers[somekey]['name']
+        email1 = customers[somekey]['email']
 
-return dict1        
-        
+return dict1
